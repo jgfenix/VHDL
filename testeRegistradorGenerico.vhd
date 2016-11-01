c5bb908367e2838eb3c102db5ad8e77252512d46
jgfenix <jgfenix@gmail.com>
2016-11-01 20:35:00 -0200

    apenas comentarios sobre os registradores

--- testeRegistradorGenerico.vhd
+++ testeRegistradorGenerico.vhd
@@ -11,10 +11,10 @@ architecture teste of TesteRegistradorGenerico is
 	signal sigin, sigout: std_logic_vector(15 downto 0);
 	signal enable, clock: std_logic;
 begin
+	-- Registrador PC simbolizando todos os outros registradores(PC, AC, IR, TIR, A, B, C, D, E, F, LA, LB)
 	PC: RegistradorGenerico port map(sigin, enable, clock, sigout);
 	clock <= '0', '1' after 10 ns, '0' after 15 ns, '1' after 20 ns, '0' after 25 ns, '1' after 30 ns, '0' after 40 ns, '0' after 50 ns, '0' after 60 ns, '1' after 70 ns;	
---	clock <= '0', '1' after 10 ns, '0' after 20 ns, '1' after 30 ns, '0' after 40 ns;	
 	sigin <= "0101010101010101", "0000000000111111" after 30 ns;
-	enable <= '1'; --, '0' after 20 ns, '1' after 30 ns, '0' after 40 ns;
+	enable <= '1';
 
 end teste;
