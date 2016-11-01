b144ee749a92a50eca70ae2fef72a1251d2a24fd
jgfenix <jgfenix@gmail.com>
2016-11-01 20:34:09 -0200

    Adicionadas teste para as constantes AMASK e SMASK

--- TesteConstantes.vhd
+++ TesteConstantes.vhd
@@ -18,15 +18,25 @@ architecture teste of TesteConstantes is
 		port(ENABLE, CK : in std_logic; SAIDA: out std_logic_vector(15 downto 0));
 	end component;
 
-	signal ENABLE, CLK: std_logic; -- sinais que são enviados para as portas de entrada
-	signal SaidaZero, SaidaUm, SaidaMenosUm: std_logic_vector (15 downto 0);
+	component ConstanteAMASK
+		port(ENABLE, CK : in std_logic; SAIDA: out std_logic_vector(15 downto 0));
+	end component;
+
+	component ConstanteSMASK
+		port(ENABLE, CK : in std_logic; SAIDA: out std_logic_vector(15 downto 0));
+	end component;
+
+	signal ENABLE, CLK: std_logic;
+	signal SaidaZero, SaidaUm, SaidaMenosUm, SaidaAMASK, SaidaSMASK: std_logic_vector (15 downto 0);
 
 begin
 	Zero: ConstanteZero port map(ENABLE, CLK, SaidaZero);
 	Um: ConstanteUm port map(ENABLE, CLK, SaidaUm);
 	MenosUm: ConstanteMenosUm port map(ENABLE, CLK, SaidaMenosUm);
+	AMASK: ConstanteAMASK port map(ENABLE, CLK, SaidaAMASK);
+	SMASK: ConstanteSMASK port map(ENABLE, CLK, SaidaSMASK);
 
-	ENABLE <= '1';
+	ENABLE <= '0', '1' after 10 ns;
 	CLK <= '0', '1' after 10 ns, '0' after 15 ns, '1' after 20 ns, '0' after 25 ns, '1' after 30 ns, '0' after 40 ns, '0' after 50 ns, '0' after 60 ns, '1' after 70 ns;
 
 end teste;
