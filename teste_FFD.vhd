8825b9aff6dc385bc5ed2064a496b0a30b0f565b
jgfenix <jgfenix@gmail.com>
2016-11-08 23:54:04 -0200

    Adicionado enable para carregar o FFD

--- teste_FFD.vhd
+++ teste_FFD.vhd
@@ -7,13 +7,14 @@ end testeflipflopD;
 
 architecture teste of testeflipflopD is
 	component flipflopD
-		port(RST, CK, D: in std_logic; Q, NQ: out std_logic);
+		port(RST, ENABLE_IN, CK, D: in std_logic; Q, NQ: out std_logic);
 	end component;
-	signal sigRST, sigCK, sigD, sigQ, sigNQ: std_logic; -- sinais que são enviados para as portas de entrada
+	signal sigRST, sigEnableIn, sigCK, sigD, sigQ, sigNQ: std_logic; -- sinais que são enviados para as portas de entrada
 begin
-	P: flipflopD port map(sigRST, sigCK, sigD, sigQ, sigNQ); -- P é um componente do tipo flipflopD
+	P: flipflopD port map(sigRST, sigEnableIn, sigCK, sigD, sigQ, sigNQ); -- P é um componente do tipo flipflopD
+	sigEnableIn <= '1'; --, '0' after 8 ns ;
 	sigRST <= '1', '1' after 10 ns, '1' after 20 ns;
-	sigCK <= '0', '1' after 3 ns, '0' after 20 ns, '1' after 30 ns, '0' after 40 ns, '1' after 50 ns;
+	sigCK <= '0', '1' after 5 ns, '0' after 20 ns, '1' after 30 ns, '0' after 40 ns, '1' after 50 ns;
 	sigD <= '1', '1' after 10 ns, '0' after 20 ns, '0' after 30 ns, '0' after 40 ns;
 
 end teste;
