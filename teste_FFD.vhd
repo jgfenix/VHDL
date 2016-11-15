69955f27d96b5a35abe779c4ef52eb6fa6885130
jgfenix <jgfenix@gmail.com>
2016-11-15 11:18:53 -0200

    Corrigido Warning 30ns -> 30 ns

--- teste_FFD.vhd
+++ teste_FFD.vhd
@@ -12,7 +12,7 @@ architecture teste of testeflipflopD is
 	signal sigRST, sigEnableIn, sigCK, sigD, sigQ, sigNQ: std_logic; -- sinais que são enviados para as portas de entrada
 begin
 	P: flipflopD port map(sigRST, sigEnableIn, sigCK, sigD, sigQ, sigNQ); -- P é um componente do tipo flipflopD
-	sigEnableIn <= '1', '0' after 18 ns, '1' after 30ns ;--usado para controlar o carregamento do bit no flipflop
+	sigEnableIn <= '1', '0' after 18 ns, '1' after 30 ns ;--usado para controlar o carregamento do bit no flipflop
 	sigRST <= '1', '1' after 10 ns, '1' after 20 ns;
 	sigCK <= '0', '1' after 5 ns, '0' after 20 ns, '1' after 30 ns, '0' after 40 ns, '1' after 50 ns;
 	sigD <= '1', '1' after 10 ns, '0' after 20 ns, '0' after 30 ns, '0' after 40 ns;
