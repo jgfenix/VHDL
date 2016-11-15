08aeafd8351aaccd085381eadb8afe9bf5e5e47a
jgfenix <jgfenix@gmail.com>
2016-11-15 11:13:33 -0200

    Arquitetura MAR refeita, agora com registrador específico e não mais adaptado um tipo D 16b.

--- MAR.vhd
+++ MAR.vhd
@@ -7,26 +7,14 @@ entity RegistradorMAR16b is
 end RegistradorMAR16b;
 
 architecture algoritmica of RegistradorMAR16b is
-	component RegistradorGenerico
-		port(ENTRADA: in std_logic_vector(15 downto 0); CK, ENABLE_IN, ENABLE_OUT_A, ENABLE_OUT_B, RST : in std_logic; SAIDA_A, SAIDA_B: out std_logic_vector(15 downto 0));
-	end component;
-	signal sigin: std_logic_vector(15 downto 0);
-	signal sigout,sigout_B: std_logic_vector(15 downto 0);
-	signal clock, ENABLE_IN, ENABLE_OUT_A, ENABLE_OUT_B, sigRST: std_logic;
+signal DADO_ARMAZENADO: std_logic_vector(15 downto 0); --usado para persistir o dado
 begin
-	MAR: RegistradorGenerico port map(sigin, clock, ENABLE_IN, ENABLE_OUT_A, ENABLE_OUT_B, sigRST, sigout, sigout_B);
-	process (ENTRADA, CONTROLE)
+	process(CONTROLE)
 	begin
-		if (CONTROLE = '0') then
-			SAIDA <= "ZZZZZZZZZZZZZZZZ";
-		else
-			ENABLE_IN <= CONTROLE; -- CONFERIR SE E CONTROLE OU '1'
-			clock <= '1', '0' after 1 ns;
-			sigin <= ENTRADA;
-			ENABLE_OUT_A <= CONTROLE;
-			ENABLE_OUT_B <= '0';
-			sigRST <= '1';
-			SAIDA <= sigout;
+		if (CONTROLE = '1') then
+			DADO_ARMAZENADO <= ENTRADA;
 		end if;
+		
 	end process;
+	SAIDA <= DADO_ARMAZENADO;
 end algoritmica;
