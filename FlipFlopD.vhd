1c610364c757f181fe0d0fc7f8012843f3218228
jgfenix <jgfenix@gmail.com>
2016-11-23 20:06:33 -0200

    Retirado enable e sig_oldD. Teste adaptado para a mudança

--- FlipFlopD.vhd
+++ FlipFlopD.vhd
@@ -3,29 +3,20 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity flipflopD is --descrição do hardware: entradas e saídas
-	port(RST, ENABLE_IN, CK, D: in std_logic; Q, NQ: out std_logic);
+	port(RST, CK, D: in std_logic; Q, NQ: out std_logic);
 end flipflopD;
 
 architecture algoritmica of flipflopD is
-signal old_D: std_logic; --usado para persistir D anterior
--- para evitar que quando o clock passar por outro ciclo, nao carregue o valor da saida do
--- barramento em D
-begin
-	process(RST, CK, D, ENABLE_IN)
-	begin
-		if (ENABLE_IN = '1') then
-			old_D <= D;
-		end if;
 
+begin
+	process(RST, CK, D)
+	begin
 		if (RST = '0') then Q <= '0'; NQ <= '1';
  
-		elsif (CK = '1' and CK'event and ENABLE_IN = '1') then
+		elsif (CK = '1' and CK'event ) then--and ENABLE_IN = '1') then
 			Q <= D;
 			NQ <= not(D);
-		elsif (CK = '1' and CK'event and ENABLE_IN = '0') then	--persistir um valor anterior da entrada D
-			Q <= old_D;
-			NQ <= not(old_D);
-		end if;	
+		end if;
 	end process;
 end algoritmica;
 
--- teste_FFD.vhd
+++ teste_FFD.vhd
@@ -7,14 +7,13 @@ end testeflipflopD;
 
 architecture teste of testeflipflopD is
 	component flipflopD
-		port(RST, ENABLE_IN, CK, D: in std_logic; Q, NQ: out std_logic);
+		port(RST, CK, D: in std_logic; Q, NQ: out std_logic);
 	end component;
-	signal sigRST, sigEnableIn, sigCK, sigD, sigQ, sigNQ: std_logic; -- sinais que são enviados para as portas de entrada
+	signal sigRST, sigCK, sigD, sigQ, sigNQ: std_logic; -- sinais que são enviados para as portas de entrada
 begin
-	P: flipflopD port map(sigRST, sigEnableIn, sigCK, sigD, sigQ, sigNQ); -- P é um componente do tipo flipflopD
-	sigEnableIn <= '1', '0' after 18 ns, '1' after 30 ns ;--usado para controlar o carregamento do bit no flipflop
+	P: flipflopD port map(sigRST, sigCK, sigD, sigQ, sigNQ); -- P é um componente do tipo flipflopD
 	sigRST <= '1', '1' after 10 ns, '1' after 20 ns;
-	sigCK <= '0', '1' after 5 ns, '0' after 20 ns, '1' after 30 ns, '0' after 40 ns, '1' after 50 ns;
+	sigCK <= '0', '1' after 5 ns, '0' after 10 ns, '1' after 15 ns, '0' after 20 ns, '1' after 25 ns, '0' after 30 ns, '1' after 35 ns, '0' after 40 ns, '1' after 45 ns;
 	sigD <= '1', '1' after 10 ns, '0' after 20 ns, '0' after 30 ns, '0' after 40 ns;
 
 end teste;
